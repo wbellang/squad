1ddd37e9dbd5597b3861174935066f4c882c3e39
wbellang <wbellang@gmail.com>
2017-09-13 13:09:16 -0500

    basic app setup

---
+++ routes/index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+var auth = require('../controllers/AuthController.js');
+
+// /* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+
+router.get('/', auth.home);
+
+router.get('/register', auth.register);
+
+router.post('/register', auth.doRegister);
+
+router.get('/login', auth.login);
+
+router.post('/login', auth.doLogin);
+
+router.get('/logout', auth.logout);
+
+module.exports = router;
